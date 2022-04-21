75ae903b1c5e84caaea3a050f66be075be31ce31
Onn Kit Kan <98507954+KanOnnKit@users.noreply.github.com>
2022-04-21 05:18:06 +0000

    Fix Typos in `main.py` and Add Fields

--- model.py
+++ model.py
@@ -1,11 +1,21 @@
 # IMPORTS
+from fields import StringField, SelectorField
 from storage import add_entry, edit_entry, find_entry
 
 
 # CLASSES
 class Entity:
-    fields = NotImplemented  # Needs to be overwritten in the subclasses
-    entity_type = NotImplemented
+    """
+    Abstract class that stores and handles the functions needed by an entity.
+    
+    This class has two class attributes:
+        - fields: the fields that will be shown when displaying to the user. 
+                  Is a dictionary mapping field name to field
+        - table_name: the table name in the database that contains the data for this entity
+    """
+    
+    fields = NotImplemented  # Needs to be overwritten in the subclasses; contains `Field` objects
+    table_name = NotImplemented
 
     # Magic methods
     def __init__(self, id_: str) -> None:
@@ -15,11 +25,16 @@ class Entity:
 
         if id_ is not None:
             # Get the data of this entity from the database
-            record = find_entry(entity_type, id_)  # Table name is the entity type and primary key is `id_`
+            record = find_entry(table_name, id_)  # Primary key is `id_`
     
             # Set the attribute values
             for key, value in record.items():
-                setattr(self, key, value)
+                # Validate value
+                if not fields[key].validate(value):
+                    # Todo do something
+                else:
+                    # Update attribute
+                    setattr(self, key, value)
 
         # Also save the PK as an attribute
         self._id = id_  # We want this to be pseudo-private
@@ -42,29 +57,33 @@ class Entity:
         # Update/Create the database entry
         if self._id is None:
             # Create a new entry
-            add_entry(entity_type, data_dict)
+            add_entry(table_name, data_dict)
         else:  # The entry already exists
-            edit_entry(entity_type, self._id, data_dict)
+            edit_entry(table_name, self._id, data_dict)
 
     def to_dict(self) -> dict:
         """
-        Converts the entity's attributes into a dictionary.
+        Converts the entity"s attributes into a dictionary.
         Note this EXCLUDES the ID that is provided to this object.
         """
 
         data_dict = {}
-        for field in fields:
+        for field in fields.keys():
             data_dict[field] = getattr(self, field)
 
         return data_dict
 
-    def validate(self):
+    def validate(self) -> bool:
         """
-        Validates the attribute values.
+        Validates the attribute values. Returns `True` if all valid, `False` otherwise.
         """
 
-        # Todo: add; do this after fields have been 
+        for key in fields.keys():
+            if not fields[key].validate(getattr(self, key)):  # Not valid value
+                return False
 
+        return True  # All checks passed
+                
     # Class methods
     @classmethod
     def from_dict(cls, dictionary: dict) -> object:
@@ -84,4 +103,31 @@ class Entity:
         return obj
 
 
-# TODO: ADD OTHER CLASSES
+class Subject(Entity):
+    """
+    Stubject entity.
+
+    Has the following fields:
+        - name: Name of the subject (string, with selections)
+        - level: Level of the subject (string, with selections)
+
+    Table name is "subject".
+    """
+
+    fields = {
+        "name": SelectorField("name", "Name of Subject", {"CLL", "PW", "MATH", "HIST", "PHY", "CHEM", "ML", "TL", "CLB", "GEO", "BENGALESE", "JAPANESE", "HINDI", "BIO", "CLTRANS", "PUNJABI", "FM", "CL", "ECONS", "GP", "ART", "ELIT", "COMP"}),
+        "level": SelectorField("level", "Subject Level", {"H1", "H2", "H3"})
+    }
+    table_name = "subject"
+
+
+# class Student(Entity):
+#     """
+#     Student entity.
+
+#     Has the following fields:
+#         - name: Name of the subject (string. with selections)
+#         - level: Level of the subject (string, with selections)
+
+#     Table name is "subject".
+#     """
