74384b67a6d29b938969e7f969d3f8d2014c5523
yxhi <77711100+yxhi@users.noreply.github.com>
2022-04-26 10:59:02 +0800

    Senior development (#16)
    * Create .gitignore
    
    * Remove system files from branch
    
    * validation.py
    
    * fields.py
    
    * change init for SelectorField
    
    * validation in fields.py
    
    * validate values in Entity.__init__
    
    Co-authored-by: KanOnnKit <98507954+KanOnnKit@users.noreply.github.com>
    Co-authored-by: JS Ng <ngjunsiang@gmail.com>

---
+++ fields.py
@@ -0,0 +1,53 @@
+import validate as valid
+
+class Field:
+    def __init__ (self, name: str, label: str, value: str):
+        self.name= name
+        self.label= label
+        
+    def __repr__(self):
+        return (
+            f'{self.__class__.__name__}('
+            f'name="{self.name}",'
+            f'label="{self.label}"'
+            ')'
+        )
+
+    def validate(self, value):
+        """Raise error if value does not pass validation"""
+        raise NotImplementedError
+        
+class IntegerField(Field):
+    def __init__(self, name: str, label: str):
+        super().__init__(name, label)
+
+    def validate(self, value):
+        if valid.validate_integer(value) is False:
+            raise valid.ValidationError(f"{value} is not a valid int")
+
+class SelectorField(Field):
+    def __init__(self, name: str, label: str, selections):
+        super().__init__(name, label)
+        self.__selections = selections
+
+    def validate(self, selection):
+        if valid.validate_selection(self.__selections, selection) is False:
+            raise valid.ValidationError(f"{selection} is not in possible selectors.")
+            
+
+class StringField(Field):
+    def __init__(self, name: str, label: str):
+        super().__init__(name, label)
+
+    def validate(self, value):
+        if valid.validate_string(value) is False:
+            raise valid.ValidationError(f"{value} is an empty string.")
+            
+class DateField(Field):
+    def __init__(self, name: str, label: str):
+        super().__init__(name, label)
+
+    def validate (self, date):
+        if valid.validate_date(date) is False:
+            raise valid.ValidationError(f"Invalid date type: {date}.")
+
